5d5a65642d74e3addf66fe772466e334b91671a3
thomas779 <57649600+thomas779@users.noreply.github.com>
2021-07-04 12:59:12 +0100

    upload main website

---
+++ js/contact.js
@@ -0,0 +1,86 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+apiKey: "AIzaSyAiNCZ_3CDNXynFas4b1UsHI6XdQwT6YcY",
+authDomain: "campus42.firebaseapp.com",
+databaseURL: "https://campus42.firebaseio.com",
+projectId: "campus42",
+storageBucket: "campus42.appspot.com",
+messagingSenderId: "1053546856481",
+appId: "1:1053546856481:web:3ae5dc4bb33914da0ea360",
+measurementId: "G-MPN1NMTLLM"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('Contact Form Responses');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+  
+    // Get values
+    var name = getInputVal('firstname');
+    var name = getInputVal('lastname');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var university = getInputVal('university');
+    var message = getInputVal('message');
+  
+    // Save message
+    saveMessage(firstname, lastname, email, phone, university, message);
+
+    // show success field
+    try {    
+      // Show alert
+      document.querySelector('.success-message').style.display = 'block';
+    
+      // Hide alert after 10 seconds
+      setTimeout(function(){
+        document.querySelector('.success-message').style.display = 'none';
+      },10000);
+    
+      // Clear form
+      document.getElementById('contactForm').reset();
+    }
+    
+    // show fail field
+    catch(err) {
+      document.getElementById("demo").innerHTML = err.message;
+      // Show alert
+      document.querySelector('.w-form-fail').style.display = 'block';
+
+      // Hide alert after 4 seconds
+      setTimeout(function(){
+        document.querySelector('.w-form-fail').style.display = 'none';
+      },4000);
+
+      // Clear form
+      document.getElementById('contactForm').reset();
+    }
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(firstname, lastname, email, phone, university, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      phone: phone,
+      university: university,
+      message: message
+    });
+  }
+  
